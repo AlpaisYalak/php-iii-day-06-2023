953b32b7bfaf01f092b0db7df300960f53f0e316
alpaisyalak <alpaisyalak7@gmail.com>
2023-07-04 12:11:14 +0100

    update hasil belajar day06

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SaCode New's</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    
+    <!-- HEADER START -->
+    <?php include ('include/header.php')?>
+    <!-- HEADER END -->
+
+
+    <!-- NAV START -->
+    <?php include ('include/nav.php')?>
+    <!-- NAV END -->
+
+
+    <!-- MAIN CONTENT START -->
+    <section class="border border-gray-800 container mx-auto p-6 text center">
+        <h1 class="font-mono text-4xl">Selamat Datang</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae, consequuntur architecto et eveniet totam natus iusto deleniti doloremque nihil.</p>
+
+        <p class="font-serif my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloremque, officiis quasi rem ut quo eos laudantium dignissimos iusto fugiat delectus saepe eaque deleniti quia molestias magni harum nulla fugit voluptatum quod at consequuntur accusamus! Nulla, cum nam praesentium facilis sequi aspernatur nihil id velit assumenda repellat! Reiciendis, iure nam.</p>
+
+        <img src="https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" width="400" alt="" srcset="">
+    </section>
+
+    <!-- MAIN CONTENT END -->
+
+
+    <!-- FOOTER START -->
+    <?php include ('include/footer.php')?>
+    <!-- FOOTER END -->
+
+
+</body>
+</html>
